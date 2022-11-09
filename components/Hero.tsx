98618852c88b4bbf6a2fceff7b02c1381eae1adf
ajachintya <40269281+ajachintya@users.noreply.github.com>
2022-11-09 20:59:29 +0530

    added nav buttons and css

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
 import BackgroundCircles from '../components/BackgroundCircles';
+import Link from 'next/link';
 
 export default function Hero() {
     const [text, count] = useTypewriter({
@@ -22,10 +23,24 @@ export default function Hero() {
           <h2 className="text-xs uppercase text-gray-500 pb-2 tracking-[15px]">
             Software Engineer
           </h2>
-          <h1 className="text-5xl lg:text-6xl font-semibold px-10">
-            <span>{text}</span>
-            <Cursor />
+          <h1 className="text-4xl lg:text-5xl font-semibold px-10">
+            <span className="mr-3">{text}</span>
+            <Cursor cursorColor="#F7AB0A" />
           </h1>
+          <div className="pt-5">
+            <Link href="#about">
+              <button className="heroButton">About</button>
+            </Link>
+            <Link href="#experience">
+              <button className="heroButton">Experiece</button>
+            </Link>
+            <Link href="#skills">
+              <button className="heroButton">Skills</button>
+            </Link>
+            <Link href="#projects">
+              <button className="heroButton">Projects</button>
+            </Link>
+          </div>
         </div>
       </div>
     );
