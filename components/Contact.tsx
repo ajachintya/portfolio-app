7ea2a2a29d2fa0c7005bb8a9128c245470a9985e
ajachintya <40269281+ajachintya@users.noreply.github.com>
2022-11-13 02:26:43 +0530

    added sanity, apis, endpoints, fetch functions

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-
 type Inputs = {
      name: string;
     subject: string;
